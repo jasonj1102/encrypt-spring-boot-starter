d70f926e305a513aad32885b1f15477a6f756146
zwj <958654064@qq.com>
2021-04-14 18:22:59 +0800

    :star: init

---
+++ src/main/java/com/github/jasonj1102/aesspringbootstarter/AesSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.jasonj1102.aesspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AesSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jasonj1102/aesspringbootstarter/AesSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jasonj1102.aesspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
