54d1ae7dacbd136aea692d8fa62b524d0a88795c
zwj <958654064@qq.com>
2021-04-15 18:58:19 +0800

    :star: 修改AES

--- src/main/java/com/github/jasonj1102/aesspringbootstarter/utils/AesUtils.java
+++ src/main/java/com/github/jasonj1102/aesspringbootstarter/utils/AesUtils.java
@@ -1,6 +1,6 @@
 package com.github.jasonj1102.aesspringbootstarter.utils;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -12,8 +12,19 @@ import java.util.Base64;
  * success come from self-discipline
  * @date 2021/4/14 22:44
  */
-@Component
 public class AesUtils {
+
+
+
+    @Value(value = "${myProperties.key}")
+    public void setSecret(String key){
+        KEY = key;
+    }
+
+    private static String KEY;
+
+
+
     private static final String AES_ALGORITHM = "AES/ECB/PKCS5Padding";
 
     /**
@@ -33,24 +44,22 @@ public class AesUtils {
     /**
      * 加密算法
      * @param data 明文
-     * @param key 密钥
      * @return String
      * @throws Exception
      */
-    public static String encrypt(String data, String key) throws Exception {
-        Cipher cipher = getCipher(key, Cipher.ENCRYPT_MODE);
+    public  String encrypt(String data) throws Exception {
+        Cipher cipher = getCipher(KEY, Cipher.ENCRYPT_MODE);
         return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
     }
 
     /**
      * 解密算法
      * @param data 密文
-     * @param key 密钥
      * @return String
      * @throws Exception
      */
-    public static String decrypt(String data, String key) throws Exception {
-        Cipher cipher = getCipher(key, Cipher.DECRYPT_MODE);
+    public  String decrypt(String data) throws Exception {
+        Cipher cipher = getCipher(KEY, Cipher.DECRYPT_MODE);
         return new String(cipher.doFinal(Base64.getDecoder().decode(data)));
     }
 }
--- src/main/java/com/github/jasonj1102/aesspringbootstarter/utils/EncryptProperties.java
+++ src/main/java/com/github/jasonj1102/aesspringbootstarter/utils/EncryptProperties.java
@@ -1,7 +1,5 @@
 package com.github.jasonj1102.aesspringbootstarter.utils;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * @author jie
@@ -9,8 +7,6 @@ import org.springframework.stereotype.Component;
  * success come from self-discipline
  * @date 2021/4/14 23:01
  */
-@Component
-@ConfigurationProperties(prefix = "spring.encrypt")
 public class EncryptProperties {
     private final static String DEFAULT_KEY = "yy<<z..ww..j>>ds";
     private String key = DEFAULT_KEY;
